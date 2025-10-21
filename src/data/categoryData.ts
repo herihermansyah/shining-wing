1791eda64da496b15d4663d41c9b17eec82b5dcc
Heri Hermansyah <heryhermansyah563@gmail.com>
2025-10-22 03:31:03 +0700

    update

--- src/data/categoryData.ts
+++ src/data/categoryData.ts
@@ -3,14 +3,14 @@ import {CategoryType} from "@/types";
 export const categoryData: CategoryType[] = [
   {
     id: "cat-01",
-    title: "new product",
-    image: "/category/cat-sneakers.png",
+    title: "new",
+    image: "/category/newproduct.png",
     path: "/",
   },
   {
     id: "cat-02",
     title: "popular",
-    image: "/category/cat-sneakers.png",
+    image: "/category/popular.png",
     path: "/",
   },
   {id: "cat-03", title: "formal", image: "/category/cat-formal.png", path: "/"},
